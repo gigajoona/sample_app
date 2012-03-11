fa6b8ac67db0cbea4abe82beeb0eb1346f9573bd
gigajoona <joona@dublao.com>
2012-03-11 14:24:40 +0200

    User model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :email
+  
+  # Does the user give a name?
+  
+  email_regex = /\A[\w+.\-]+@[a-z\d.\-]+\.[a-z]+\z/i
+  
+  validates :name, :presence => true,
+                   :length => { :maximum => 50 }
+  validates :email, :presence => true,
+                    :format => { :with => email_regex },
+                    :uniqueness => {:case_sensitive => false}
+  
 end
