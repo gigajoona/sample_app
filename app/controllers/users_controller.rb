566c173153974d98666ffd5526829a87c558a483
gigajoona <joona@dublao.com>
2012-03-16 16:27:34 +0200

    Finished user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,5 +10,15 @@ class UsersController < ApplicationController
     @title = "Sign up"
   end
   
+  def create
+   @user = User.new(params[:user])
+   if @user.save
+     flash[:success] = "See, it works!"
+     redirect_to @user
+   else  
+     @title = "Sign up"
+     render 'new'
+   end
+  end
  
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -53,6 +53,56 @@ describe UsersController do
     response.should have_selector('title', :content => "Sign up")
   end
   
+  describe "POST 'create'" do
+  
+    describe "failure" do
+    
+      before(:each) do
+        @attr = { :name => "", :email => "", :password => "", :password_confirmation => ""}
+      
+      end
+  
+      it "should have the right title" do
+        post :create, :user => @attr
+        response.should have_selector('title', :content => "Sign up")
+      end
+      
+      it "should render a 'new' page" do
+        post :create, :user => @attr
+        response.should render_template('new')
+      end
+      
+      it "should not create a user" do
+        lambda do
+          post :create, :user => @attr
+        end.should_not change(User, :count)
+      
+      end
+    end
+    
+  describe "success" do
+    
+    before(:each) do
+      @attr = { :name => "New User", :email => "user@example.com", :password => "foobar", :password_confirmation => "foobar"}
+    end 
+
+    it "should create a user" do
+      lambda do
+        posts :create, :user => @attr
+      end.should change(User, :count).by(1)
+    end
+    
+    it "should redicet to the user show page" do
+      post :create, :user => @attr
+      response.should redirect_to(user_path(assigns(:user)))
+    end
+    
+    it "should have a welcome message" do
+      post :create, :user => @attr
+      flash.[:success].should =~ / See, it works!/i
+    end
+    
+  end
   end
 
 end
