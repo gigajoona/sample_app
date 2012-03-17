ee728f84b1ccbdcd4c6263b4a226c2ae3c02acb4
gigajoona <joona@dublao.com>
2012-03-17 12:15:07 +0200

    Handling invalid signin

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -99,7 +99,7 @@ describe UsersController do
     
     it "should have a welcome message" do
       post :create, :user => @attr
-      flash.[:success].should =~ / See, it works!/i
+      flash[:success].should =~ /See, it works!/i
     end
     
   end
